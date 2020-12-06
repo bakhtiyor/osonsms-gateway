8349beb5d421192494076102a55a3e1cc3c92c31
Bakhtiyor Bahriddinov <bakhtiyor.bahriddinov@gmail.com>
2020-12-06 22:56:51 +0500

    Created a config file for the package

--- config/config.php
+++ config/config.php
@@ -4,5 +4,8 @@
  * You can place your custom package configuration in here.
  */
 return [
-
+    'login' => 'YOUR_LOGIN',                                    // your login in OsonSMS platform
+    'hash' => 'YOUR_HASH',                                      // your hash code in OsonSMS platform
+    'sender' => 'OsonSMS',                                      // the name of your SMS sender
+    'server_url' => 'http://api.osonsms.com/sendsms_v1.php'     // OsonSMS api server URL
 ];
